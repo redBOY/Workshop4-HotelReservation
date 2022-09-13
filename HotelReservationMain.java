b5cbf543c071cde1e3d1a5a9e3ba8d60c6c2b0eb
“Adithya <adithyasrinivas66@gmail.com>
2022-09-13 18:35:44 +0530

    uc1 commit

--- HotelReservationMain.java
+++ HotelReservationMain.java
@@ -3,8 +3,8 @@ package bridgelabzWorkshop4HotelReservation;
 public class HotelReservationMain {
     public static void main(String[] args) {
         System.out.println("welcome to HotelReservation program");
-        HotelReservationMain hotelReservation = new HotelReservationMain();
-        hotelReservation.addHotel(new Hotel("Lakewood",110);
+        HotelReserveService hotelReservation = new HotelReserveService();
+        hotelReservation.addHotel(new Hotel("Lakewood",110));
         hotelReservation.addHotel(new Hotel("Ridgwood",220));
     }
 }
